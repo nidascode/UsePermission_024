1c922a9f1b05f1eb19b8064084df8911295efd7a
Nida Shafique <nidascode@gmail.com>
2022-05-17 15:59:37 +0500

    uses permission

--- app/src/main/java/com/example/usepermission_024/MainActivity.java
+++ app/src/main/java/com/example/usepermission_024/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.usepermission_024;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void use_bt_action(View v){
+        if(checkSelfPermission("com.example.dangerousactivity.MY_DANG_PERM") != PackageManager.PERMISSION_GRANTED)
+        {
+            requestPermissions(new String[]{"com.example.sectionc_024_MyPermission"}, 1);
+        }
+        else
+        {
+            Intent intent = new Intent();
+            intent.setAction("com.example.sectionc_024_MyPermission");
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch(requestCode)
+        {
+            case 1:
+                Intent intent = new Intent();
+                intent.setAction("com.example.sectionc_024_MyPermission");
+                startActivity(intent);
+
+        }
     }
 }
